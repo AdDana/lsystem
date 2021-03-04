d411961c1fb9d80d5bfb87f1a7ecceeb5269b148
Kvantorium <magkihsveta1@gmail.com>
2021-03-04 16:35:53 +0300

    Добавьте файлы проекта.

---
+++ Project4/MyForm.cpp
@@ -0,0 +1,140 @@
+﻿#include "MyForm.h"
+#include <iostream>
+#include <math.h>
+
+#include <Windows.h>
+#include <string>
+#include <msclr\marshal.h>
+#include <msclr\marshal_cppstd.h>
+
+using namespace msclr::interop;
+using namespace Project4;
+using namespace std;
+
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	return 0;
+}
+
+
+string kk = "";
+int iteration;
+
+void Project4::MyForm::parser()
+{
+int a,r=0; //кол-во итер
+
+iteration = (int)Convert::ToDouble(textBox1->Text);
+
+//a = Int32::Parse(textBox1->Text);
+
+String^ pravilo = textBox4->Text;//F+F-F-F+F
+string k;
+a = k.length();
+string basestring;
+
+
+
+
+k=marshal_as<string>(textBox5->Text);
+
+for (int j = 0; j < iteration; j++)
+{
+	a = k.length();
+	for (int i = 0; i < a; i++)
+	{		
+
+		switch (k[i])
+		{
+		case 'F':
+			//kk += k;
+			//k.erase(i, 1);
+			kk+= marshal_as<string>(textBox4->Text);//F+F-F-F+F
+			break;
+		case '+':
+			kk += '+';
+			//kk.insert(i, "+");
+			break;
+		case '-':
+			kk += '-';
+			//kk.insert(i, "-");
+			break;
+		default:
+
+			break;
+
+		}
+		
+	}
+	//a = r;
+	k = kk;	
+	kk = "";	
+}
+kk = k;
+}
+
+
+//Graphics ^ g = pictureBox1->CreateGraphics();
+
+void Project4::MyForm::drawline(int x1, int y1,int angle)
+{
+	double pi = acos(-1);
+	double radians = angle / 180.0 * pi;
+
+	double endx = cos(radians)*delta;
+	double endy = sin(radians)*delta;
+
+
+	
+	Pen ^ blackPen = gcnew Pen(Color::Black, 0.5);
+	
+	g->DrawLine(blackPen, x1, y1, x1+endx, y1+endy);
+	
+	base_x = x1 + endx;
+	base_y = y1 + endy;
+}
+
+
+void Project4::MyForm::drawtree()
+{
+	//kk.insert(0, marshal_as<string>(textBox5->Text));
+
+	System::String^ fd = marshal_as<System::String^>(kk);
+	textBox2->Text = fd;
+
+	
+		for (int i = 0; i < kk.length(); i++)
+		{
+
+			switch (kk[i])
+			{
+			case 'F':
+
+				drawline(base_x, base_y, baseangle);
+
+				break;
+			case '+':
+
+				baseangle += 90;
+
+				break;
+			case '-':
+
+				baseangle -= 90;
+
+				break;
+			default:
+				break;
+
+			}
+
+		
+
+	}
+
+
+
+}
---
+++ Project4/MyForm.h
@@ -0,0 +1,268 @@
+#pragma once
+
+namespace Project4 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private: System::Windows::Forms::TextBox^  textBox1;
+
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::TextBox^  textBox5;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Button^  button1;
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(42, 87);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->Text = L"2";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(42, 232);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(281, 296);
+			this->textBox2->TabIndex = 3;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(42, 29);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"1 Действие";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(356, 29);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(911, 604);
+			this->pictureBox1->TabIndex = 5;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(42, 58);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"2 Действие";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(42, 113);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 7;
+			this->textBox3->Text = L"90";
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(42, 165);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 8;
+			this->textBox4->Text = L"F+F-F-F+F";
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(42, 139);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(100, 20);
+			this->textBox5->TabIndex = 9;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(174, 90);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(56, 13);
+			this->label1->TabIndex = 10;
+			this->label1->Text = L"Итерации";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(174, 116);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(32, 13);
+			this->label2->TabIndex = 11;
+			this->label2->Text = L"Угол";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(174, 142);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(44, 13);
+			this->label3->TabIndex = 12;
+			this->label3->Text = L"Начало";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(174, 168);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(51, 13);
+			this->label4->TabIndex = 13;
+			this->label4->Text = L"Правило";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(275, 29);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 14;
+			this->button1->Text = L"Стереть";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1292, 669);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"    ";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	public:
+		
+		int baseangle = 0;
+		void parser();
+		
+
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		baseangle=(int)Convert::ToDouble(textBox3->Text);
+		parser();
+	}
+
+#pragma endregion
+
+
+			 int delta = 5;
+			 int x1 = 300, y1 = 300;
+			 int x2, y2;			 
+			 int base_x = 300;
+			 int base_y = 300;
+		
+			Graphics ^ g;
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+public:
+	void drawline(int x1, int y1, int angle);
+	void drawtree();
+
+
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+	g = pictureBox1->CreateGraphics();
+	
+	drawtree();
+}
+private: System::Void button1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	g->Clear(SystemColors::Control);
+	x1 = 300;
+	y1 = 300;
+	base_x = 300;
+    base_y = 300;
+	baseangle = 0;
+}
+};
+}
